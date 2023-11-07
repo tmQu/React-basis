0c74910b6bf21b59c9f139156221061d1a5e33d8
MQ <tmquang21@clc.fitus.edu.vn>
2023-11-07 15:15:37 +0700

    create component

---
+++ create-component/src/App.js
@@ -0,0 +1,21 @@
+
+
+function Heading()
+{
+  return (
+    <h1>This is a h1 heading</h1>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>this is the starting code</h1>
+      
+      <Heading />
+    </div>
+    
+  )
+}
+
+export default App;
