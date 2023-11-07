5ced95dcf5d5a0aa1abd83a669ad329fdad2da77
MQ <tmquang21@clc.fitus.edu.vn>
2023-11-07 15:19:34 +0700

    create import component

--- create-component/src/App.js
+++ create-component/src/App.js
@@ -1,17 +1,12 @@
 
 
-function Heading()
-{
-  return (
-    <h1>This is a h1 heading</h1>
-  );
-}
+import Heading from './Heading'
 
 function App() {
   return (
     <div>
       <h1>this is the starting code</h1>
-      
+
       <Heading />
     </div>
     
